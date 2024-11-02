a22311e71d3bdd3416e6a49d1d78990419efb6e9
sourav-py <imsrv2k@gmail.com>
2024-11-02 22:09:21 +0530

    POC: Fetch pods corresponding to a namespace from a local cluster

--- go/playground/go.mod
+++ go/playground/go.mod
@@ -1,8 +1,11 @@
-module github.com/sourav-py/klusterduck/go/test
+module github.com/sourav-py/klusterduck/go/playground
 
 go 1.23.1
 
-require k8s.io/client-go v0.31.2
+require (
+	k8s.io/apimachinery v0.31.2
+	k8s.io/client-go v0.31.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -38,7 +41,6 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.31.2 // indirect
-	k8s.io/apimachinery v0.31.2 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
---
+++ go/test/list_pods.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	// Use kubeconfig file from ~/.kube/config by default, or the one specified via `-kubeconfig` flag
+	kubeconfig := filepath.Join(homeDir(), ".kube", "config")
+	configPath := flag.String("kubeconfig", kubeconfig, "(optional) absolute path to the kubeconfig file")
+	flag.Parse()
+
+	// Build the configuration from the kubeconfig file
+	config, err := clientcmd.BuildConfigFromFlags("", *configPath)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Create a new clientset to interact with the Kubernetes API
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ListPodsInNamespace(clientset, "kube-system")
+
+}
+
+// homeDir returns the home directory of the user running this program
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // Windows
+}
+
+func ListPods(clientset *kubernetes.Clientset) {
+	//List all the pods in the cluster
+	fmt.Println("Pods in the cluster: ")
+
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	itr := 1
+	for _, pod := range pods.Items {
+		//Print Name and Namespace of the pod
+		fmt.Printf("%d Pod name: %s, Namesapce: %s\n", itr, pod.Name, pod.Namespace)
+
+		//Print labels for the pod
+		for label, value := range pod.Labels {
+			fmt.Printf("\t%s:%s\n", label, value)
+		}
+
+		itr += 1
+	}
+
+}
+
+// List all the pods in a namespace
+func ListPodsInNamespace(clientset *kubernetes.Clientset, namespace string) {
+	fmt.Printf("Pods in the '%s' namespace\n", namespace)
+
+	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	itr := 1
+	for _, pod := range pods.Items {
+		fmt.Printf("%d Pod name: %s, Namespace: %s\n", itr, pod.Name, pod.Namespace)
+		itr += 1
+	}
+}
+
+// List all services in the cluster
+func ListServices(clientset *kubernetes.Clientset) {
+	fmt.Println("Services in the cluster:")
+
+	services, err := clientset.CoreV1().Services("").List(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, svc := range services.Items {
+		fmt.Printf("Service Name: %s, Namespace: %s, Type: %s\n", svc.Name, svc.Namespace, svc.Spec.Type)
+	}
+}
